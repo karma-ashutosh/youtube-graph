ed5b49f99c4699880e880c03b897e5e1d18b882e
Ashutosh Vishwakarma <karma@Ashutoshs-MacBook-Air.local>
2025-11-07 18:45:27 +0530

    updated deployment screen

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 docker build --platform linux/amd64 -t us-central1-docker.pkg.dev/big-website-445809/youtube-graph/youtube-graph-app:latest .
 docker push us-central1-docker.pkg.dev/big-website-445809/youtube-graph/youtube-graph-app:latest
+
+echo "sleeping"
+sleep 30
+
+echo "awake"
 gcloud run services update youtube-graph --region us-central1 --image us-central1-docker.pkg.dev/big-website-445809/youtube-graph/youtube-graph-app:latest
